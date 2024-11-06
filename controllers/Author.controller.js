7d81520dbf14bd2223a0582fb7ab42dd79db6672
Komron Mamataliyev <komronmamataliyev@gmaill.com>
2024-11-06 20:56:25 +0500

    jigar

---
+++ controllers/Author.controller.js
@@ -0,0 +1,7 @@
+export const GetAllAuthor = (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
--- routes/Author.routes.js
+++ routes/Author.routes.js
@@ -1,12 +1,9 @@
 import {Router} from "express";
 import {} from "../controllers/index.js";
-
-
-
-
 const AuthorRouter = Router()
 
 
+AuthorRouter.get("/", GetAllAuthor);
 AuthorRouter.get("/:id", GetAuthorByid);
 AuthorRouter.post("/", CreateAuthor);
 AuthorRouter.put("/:id", UpdateAuthor);
