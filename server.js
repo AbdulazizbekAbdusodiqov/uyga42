9150741b9919bebdf26a591c472b8e7dc557bb01
Abdulaziz_c5 <abdulazizbekabdusodiqov@gmail.com>
2024-11-06 19:47:14 +0500

    commit

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { config } from "dotenv";
+
+config()
+
+const PORT = process.env.PORT
+
+const app = express()
+app.use(express.json())
+
+
+
+app.listen(PORT, ()=>{
+    console.log("PORT:" + PORT)
+})
